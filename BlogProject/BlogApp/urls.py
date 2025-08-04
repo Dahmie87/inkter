11cf58cb3dee7bd039307e78f7c48e6e0e0b66e1
Dahmie <omotayodamilare07@gmail.com>
2025-08-04 15:20:57 +0100

    add dynamic urls

--- BlogProject/BlogApp/urls.py
+++ BlogProject/BlogApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.home, name="home"),
-    path("post/<int:ID>", views.postpage),
-    path("create", views.create)
+    path("post/<int:ID>", views.postpage, name="post-full"),
+    path("create", views.create, name="create")
 ]
