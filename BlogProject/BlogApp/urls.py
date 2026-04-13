98b7830af8f81b7e235eec989ff0576fb7d2b451
Damilare Omotayo <omotayodamilare07@gmail.com>
2026-04-13 07:38:41 +0100

    Update urls.py

--- BlogProject/BlogApp/urls.py
+++ BlogProject/BlogApp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("post/<int:ID>", views.postpage, name="post-full"),
-    path("create", views.create, name="create")
+    path("", views.index, name="index"),
+    path("post/<int:post_id>/", views.post_full, name="post-full"),
+    path("create/", views.create, name="create"),
+    path("ready/<int:post_id>/", views.ready, name="ready"),
 ]
