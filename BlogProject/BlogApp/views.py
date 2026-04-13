2129f70cdf06f6a50a3a076bc984d494be6e165b
Damilare Omotayo <omotayodamilare07@gmail.com>
2026-04-13 07:38:16 +0100

    Update views.py

--- BlogProject/BlogApp/views.py
+++ BlogProject/BlogApp/views.py
@@ -14,28 +14,32 @@ eduimglist = ["edu1", "edu2", "edu3"]
 foodimglist = ["food1", "food2", "food3", "food4"]
 
 
-def home(request):
+def index(request):
+    """Homepage - display all posts"""
     posts = post.objects.all()
     imgname = "postimg"
     return render(request, "index.html", {'posts': posts, 'imgname': imgname})
 
 
-def postpage(request, ID):
-    page = post.objects.get(id=ID)
-
+def post_full(request, post_id):
+    """Display full post detail page"""
+    page = post.objects.get(id=post_id)
     return render(request, 'post.html', {'page': page})
 
 
 def create(request):
-
+    """Create a new post - GET shows form, POST saves post"""
     if request.method == "GET":
         return render(request, "create.html")
+    
     elif request.method == "POST":
         new_post = request.POST.get("postcreated")
         new_title = request.POST.get("newtitle")
         writer = request.POST.get("writer")
         time_made = datetime.now()
         tagname = request.POST.get("tag")
+        
+        # Select random image based on category
         if tagname == "tech":
             imgname = random.choice(techimglist)
         elif tagname == "bus":
@@ -50,7 +54,27 @@ def create(request):
             imgname = random.choice(eduimglist)
         elif tagname == "food":
             imgname = random.choice(foodimglist)
+        else:
+            imgname = "default"
 
-        new_post = post.objects.create(title=new_title, content=new_post,
-                                       author=writer, time=time_made, picture=imgname+".jpg")
+        # Create the post
+        new_post = post.objects.create(
+            title=new_title, 
+            content=new_post,
+            author=writer, 
+            time=time_made, 
+            picture=imgname + ".jpg",
+            tag=tagname
+        )
+        
+        # Redirect to success page
         return render(request, "ready.html", {'post_id': new_post.id})
+
+
+def ready(request, post_id):
+    """Success page after posting"""
+    try:
+        new_post = post.objects.get(id=post_id)
+        return render(request, "ready.html", {'post': new_post, 'post_id': post_id})
+    except post.DoesNotExist:
+        return render(request, "ready.html", {'post_id': post_id})
